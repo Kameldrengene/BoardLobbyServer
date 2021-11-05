dd92145c54f85960df181a7062c7bf20ef9cc085
Volkan Isik <s180103@student.dtu.dk>
2021-11-05 12:59:25 +0100

    admin crud

--- BoardLobbyServer/Pages/Index.cshtml.cs
+++ BoardLobbyServer/Pages/Index.cshtml.cs
@@ -5,13 +5,16 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace BoardLobbyServer.Pages
 {
     public class IndexModel : PageModel
     {
-        
+        public string weather { get; set; } = "";
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -19,16 +22,39 @@ namespace BoardLobbyServer.Pages
             _logger = logger;
         }
 
-        public IActionResult OnGet()
+        public async Task OnGet()
+        {
+           weather = await GetWeather();
+        }
+        public async Task<string> GetWeather()
         {
 
-            
-            if (HttpContext.Session.GetString("LoggedIn")==null)
+            using (var client = new HttpClient())
             {
-                return Redirect("SignIn");
+                string vejr = null;
+             
+                var request = new HttpRequestMessage(HttpMethod.Get, "http://vejr.eu/api.php?location=Roskilde&degree=C");
+                var header1 = new MediaTypeWithQualityHeaderValue("application/json");
+                var header2 = new ProductInfoHeaderValue("CamelLudo", "1.0");
+
+                request.Headers.UserAgent.Add(header2);
+                request.Headers.Accept.Add(header1);
+
+                
+                var resp = await client.SendAsync(request);
+
+               
+                if (resp.IsSuccessStatusCode)
+                {
+
+                     vejr = resp.Content.ReadAsStringAsync().Result;
+
+                }
+
+                
+                //returning the student list to view  
+                return vejr;
             }
-            return null;
-            
         }
     }
 }
