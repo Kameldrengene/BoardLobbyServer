5c87737b7085c8b63c273f561b90c06e36c53c70
Volkan Isik <s180103@student.dtu.dk>
2021-11-13 01:53:21 +0100

    master account

--- BoardLobbyServer/Pages/SignUp.cshtml.cs
+++ BoardLobbyServer/Pages/SignUp.cshtml.cs
@@ -12,6 +12,8 @@ namespace BoardLobbyServer.Pages
 {
     public class SignUpModel : PageModel
     {
+        public bool master { get; private set; } = false;
+        public bool firstTime { get; private set; } = false;
         private readonly AdminService _adminService;
         public string log { get; set; } = "";
 
@@ -21,6 +23,22 @@ namespace BoardLobbyServer.Pages
         }
         public void OnGet()
         {
+            if (_adminService.Get().Count == 0)
+            {
+                firstTime = true;
+            }
+            else
+            {
+                if (HttpContext.Session.GetString("LoggedIn") != null)
+                {
+                    Admin admin = _adminService.GetByName(HttpContext.Session.GetString("LoggedIn"));
+                    if (admin.AdminType == Admin.Type.Master)
+                    {
+                        master = true;
+                    }
+                }
+            }
+
         }
         public IActionResult OnPost(string emailAddress, string password)
         {
@@ -29,6 +47,10 @@ namespace BoardLobbyServer.Pages
                 Admin admin = new Admin();
                 admin.Name = emailAddress;
                 admin.Password = password;
+                if (_adminService.Get().Count == 0)
+                {
+                    admin.AdminType = Admin.Type.Master;
+                }
                 _adminService.Create(admin);
                 HttpContext.Session.SetString("LoggedIn", admin.Name);
                 return Redirect("Index");
