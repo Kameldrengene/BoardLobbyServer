0f45e808b50daef68bb032a50f7953633224de94
Lizator <frederikkoefoeds@gmail.com>
2021-12-10 20:09:06 +0100

    added field quadrant to pieceData

--- BoardLobbyServer/Model/PieceData.cs
+++ BoardLobbyServer/Model/PieceData.cs
@@ -11,14 +11,16 @@ namespace BoardLobbyServer.Model
         private PieceColor _pieceColor;
         private int _pieceID;
         private int _fieldID;
+        private int _fieldQuadrant;
         private bool _isInPlay;
         private bool _isDone;
 
-        public PieceData(PieceColor pc, int pID, int fID, bool inPlay, bool Done)
+        public PieceData(PieceColor pc, int pID, int fID, int fQ, bool inPlay, bool Done)
         {
             this._pieceColor = pc;
             this._pieceID = pID;
             this._fieldID = fID;
+            this._fieldQuadrant = fQ;
             this._isInPlay = inPlay;
             this._isDone = Done;
         }
@@ -26,6 +28,7 @@ namespace BoardLobbyServer.Model
         public PieceColor PieceColor { get => _pieceColor; set => _pieceColor = value; }
         public int PieceID { get => _pieceID; set => _pieceID = value; }
         public int FieldID { get => _fieldID; set => _fieldID = value; }
+        public int FieldQuadrant { get => _fieldQuadrant; set => _fieldQuadrant = value; }
         public bool IsInPlay { get => _isInPlay; set => _isInPlay = value; }
         public bool IsDone { get => _isDone; set => _isDone = value; }
     }
