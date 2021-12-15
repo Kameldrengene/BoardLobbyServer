4f1142f09f99680b2e16fa6a46f394b0b54e108c
Volkan Isik <s180103@student.dtu.dk>
2021-12-15 01:06:49 +0100

    backend forbedringer

---
+++ BoardLobbyServer/Services/Validator.cs
@@ -0,0 +1,15 @@
+﻿using BoardLobbyServer.Model;
+using System;
+using System.Text.RegularExpressions;
+
+namespace BoardLobbyServer.Services
+{
+    public class Validator
+    {
+        public bool validateAdmin(Func<Admin,bool> validateEmail, Func<Admin, bool> validatePassword,Admin admin)
+        {
+            bool result = validateEmail(admin) && validatePassword(admin);
+            return result;
+        }
+    }
+}
